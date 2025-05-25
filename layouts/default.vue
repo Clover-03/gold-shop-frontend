433fbc98ff2289512d99e15f5e96652e61b96739
Clover-03 <163443565+Clover-03@users.noreply.github.com>
2025-05-25 19:37:54 +0700

    Image

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,42 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      class="bg-grey-lighten-4"
-    >
-      <v-list-item class="bg-blue-darken-4 text-white">
-        <v-list-item-content>
-          <v-list-item-title class="text-h6 font-weight-bold">
-            Dalavong Jelwery
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Dalavong Jewelry</v-toolbar-title>
+    </v-app-bar>
 
+    <v-navigation-drawer
+      app
+      permanent
+      class="bg-grey-lighten-4 rounded-b-xl"
+    >
       <v-divider />
 
       <v-list nav density="comfortable">
-        <v-list-item
-          to="/"
-          prepend-icon="mdi-account"
-          :class="{ 'active-link': isActive('/') }"
-        >
+        <v-list-item to="/" :class="{ 'active-link': isActive('/') }">
+          <template #prepend>
+            <v-img src="/icons/Profile.png" width="24" height="24" cover class="mr-2" />
+          </template>
           ໂປຣໄຟລ໌
         </v-list-item>
-        <v-list-item
-          to="/home"
-          prepend-icon="mdi-home"
-          :class="{ 'active-link': isActive('/home') }"
-        >
+
+        <v-list-item to="/home" :class="{ 'active-link': isActive('/home') }">
+          <template #prepend>
+            <v-img src="/icons/Home.png" width="24" height="24" cover class="mr-2" />
+          </template>
           ໜ້າຫຼັກ
         </v-list-item>
 
-        <!-- ຈັດການຂໍ້ມູນ -->
-        <v-list-group prepend-icon="mdi-folder-cog" value="manage">
-          <template #activator="{ props }">
-            <v-list-item v-bind="props" title="ຈັດການຂໍ້ມູນ" />
+        <v-list-group value="manage">
+          <template #activator="{ props, isOpen }">
+            <v-list-item v-bind="props">
+              <template #prepend>
+                <v-img src="/icons/Manage.png" width="24" height="24" cover class="mr-2" />
+              </template>
+              ຈັດການຂໍ້ມູນ
+              <template #append>
+                <v-img src="/icons/Arrow.png" width="16" height="16" cover class="ml-auto transition-transform" :class="{ 'rotate-180': isOpen }" />
+              </template>
+            </v-list-item>
           </template>
           <v-list-item to="/manage/product" title="ຈັດການຂໍ້ມູນສິນຄ້າ" :class="{ 'active-link': isActive('/manage/product') }" />
           <v-list-item to="/manage/goldprice" title="ຈັດການຂໍ້ມູນລາຄາຄຳ" :class="{ 'active-link': isActive('/manage/goldprice') }" />
@@ -42,29 +44,50 @@
           <v-list-item to="/manage/supplier" title="ຈັດການຂໍ້ມູນຜູ້ສະໜອງ" :class="{ 'active-link': isActive('/manage/supplier') }" />
         </v-list-group>
 
-        <!-- ການຈັດການຊື້ -->
-        <v-list-group prepend-icon="mdi-cart-arrow-down" value="buy">
-          <template #activator="{ props }">
-            <v-list-item v-bind="props" title="ການຈັດການຊື້" />
+        <v-list-group value="buy">
+          <template #activator="{ props, isOpen }">
+            <v-list-item v-bind="props">
+              <template #prepend>
+                <v-img src="/icons/purchase.png" width="24" height="24" cover class="mr-2" />
+              </template>
+              ການຈັດການຊື້
+              <template #append>
+                <v-img src="/icons/Arrow.png" width="16" height="16" cover class="ml-auto transition-transform" :class="{ 'rotate-180': isOpen }" />
+              </template>
+            </v-list-item>
           </template>
           <v-list-item to="/buy/orders" title="ຈັດການຂໍ້ມູນການສັ່ງຊື້" :class="{ 'active-link': isActive('/buy/orders') }" />
           <v-list-item to="/buy/delivery" title="ຈັດການຂໍ້ມູນການນໍາສົ່ງສິນຄ້າ" :class="{ 'active-link': isActive('/buy/delivery') }" />
           <v-list-item to="/buy/returns" title="ຈັດການຂໍ້ມູນການຊື້ຄືນສິນຄ້າ" :class="{ 'active-link': isActive('/buy/returns') }" />
         </v-list-group>
 
-        <!-- ການຈັດການຂາຍ -->
-        <v-list-group prepend-icon="mdi-cart-arrow-right" value="sell">
-          <template #activator="{ props }">
-            <v-list-item v-bind="props" title="ການຈັດການຂາຍ" />
+        <v-list-group value="sell">
+          <template #activator="{ props, isOpen }">
+            <v-list-item v-bind="props">
+              <template #prepend>
+                <v-img src="/icons/Sell.png" width="24" height="24" cover class="mr-2" />
+              </template>
+              ການຈັດການຂາຍ
+              <template #append>
+                <v-img src="/icons/Arrow.png" width="16" height="16" cover class="ml-auto transition-transform" :class="{ 'rotate-180': isOpen }" />
+              </template>
+            </v-list-item>
           </template>
           <v-list-item to="/sell/products" title="ຈັດການຂໍ້ມູນການຂາຍສິນຄ້າ" :class="{ 'active-link': isActive('/sell/products') }" />
           <v-list-item to="/sell/exchanges" title="ຈັດການຂໍ້ມູນການປ່ຽນສິນຄ້າ" :class="{ 'active-link': isActive('/sell/exchanges') }" />
         </v-list-group>
 
-        <!-- ລາຍງານ -->
-        <v-list-group prepend-icon="mdi-file-chart" value="report">
-          <template #activator="{ props }">
-            <v-list-item v-bind="props" title="ລາຍງານ" />
+        <v-list-group value="report">
+          <template #activator="{ props, isOpen }">
+            <v-list-item v-bind="props">
+              <template #prepend>
+                <v-img src="/icons/Report.png" width="24" height="24" cover class="mr-2" />
+              </template>
+              ລາຍງານ
+              <template #append>
+                <v-img src="/icons/Arrow.png" width="16" height="16" cover class="ml-auto transition-transform" :class="{ 'rotate-180': isOpen }" />
+              </template>
+            </v-list-item>
           </template>
           <v-list-item to="/report/customers" title="ລາຍງານຂໍ້ມູນລູກຄ້າ" :class="{ 'active-link': isActive('/report/customers') }" />
           <v-list-item to="/report/suppliers" title="ລາຍງານຂໍ້ມູນຜູ້ສະໜອງ" :class="{ 'active-link': isActive('/report/suppliers') }" />
@@ -81,7 +104,10 @@
       <template #append>
         <v-divider></v-divider>
         <v-list>
-          <v-list-item prepend-icon="mdi-logout" @click="logout">
+          <v-list-item @click="logout">
+            <template #prepend>
+              <v-img src="/icons/Logout.png" width="24" height="24" cover class="mr-2" />
+            </template>
             ອອກຈາກລະບົບ
           </v-list-item>
         </v-list>
@@ -97,14 +123,10 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import { useRoute } from 'vue-router'
-
-const drawer = ref(true)
 const route = useRoute()
 
-const isActive = (path) => route.path === path
-
+const isActive = (path) => route.path.startsWith(path)
 
 const logout = () => {
   console.log("Logout")
@@ -116,5 +138,8 @@ const logout = () => {
   background-color: #295396 !important;
   color: white !important;
   border-radius: 8px;
+}
+.rotate-180 {
+  transform: rotate(180deg);
 }
 </style>
