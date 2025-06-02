4c4f5cd25093d85d9c59a839771fbd4afac81454
Clover-03 <163443565+Clover-03@users.noreply.github.com>
2025-06-02 19:50:52 +0700

    image product and  page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 export default defineNuxtConfig({
-  css: ['vuetify/styles'],
+  css: [
+    'vuetify/styles',
+    '@/assets/styles/main.css' // เพิ่มบรรทัดนี้
+  ],
   build: {
     transpile: ['vuetify'],
   },
